f3c9f5d7ea6fab84e1351cc161fe236e5f0baa3d
chalkebs <51581618+chalkebs@users.noreply.github.com>
2019-07-03 17:08:10 +0530

    Adding new code

---
+++ PrimeAngPal.java
@@ -0,0 +1,11 @@
+package com.bridgelab;
+import util.Utility;
+
+public class PrimeAngPal 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Prime no. are...");
+		Utility.Prime1();
+	}
+}
